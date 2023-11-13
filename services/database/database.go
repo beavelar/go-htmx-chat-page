c2637d7cf37754f7da5c7cb162a62591ee11c9e7
beavelar <beavelar.github@gmail.com>
2023-11-12 22:46:07 -0700

    Format using LSP

--- services/database/database.go
+++ services/database/database.go
@@ -1,20 +1,20 @@
 package main
 
 import (
-    "database/sql"
-    "fmt"
-    "log"
+	"database/sql"
+	"fmt"
+	"log"
 
-    _ "github.com/lib/pq"
+	_ "github.com/lib/pq"
 )
 
 var Db *sql.DB
 
 func InitDb(host string, name string, user string, pass string) error {
-    log.Printf("initializing db connection - host: %s, name: %s, user: %s\n", host, name, user)
+	log.Printf("initializing db connection - host: %s, name: %s, user: %s\n", host, name, user)
 
-    var err error
-    connStr := fmt.Sprintf("connect_timeout=10 dbname=%s host=%s user=%s password=%s", name, host, user, pass)
-    Db, err = sql.Open("postgres", connStr)
-    return err
+	var err error
+	connStr := fmt.Sprintf("connect_timeout=10 dbname=%s host=%s user=%s password=%s", name, host, user, pass)
+	Db, err = sql.Open("postgres", connStr)
+	return err
 }
